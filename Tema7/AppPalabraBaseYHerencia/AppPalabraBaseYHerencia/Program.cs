123ca76b2506d4c95c0a4c2638e295b72e50b186
Pablo Lopez <esviasalpa@gmail.com>
2018-01-15 10:23:32 +0100

    Ejemplos de propiedades,herencia y palabras reservadas BASE y NEW

---
+++ Tema7/AppPalabraBaseYHerencia/AppPalabraBaseYHerencia/Program.cs
@@ -0,0 +1,93 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola: en este ejemplo se trata palabras clave como " BASE y NEW" ademas de la herencia
+ Fecha:15/01/2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPalabraBaseYHerencia.pbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LienzoImpresora lip = new LienzoImpresora();
+            lip.Dibuja();
+
+            Console.ReadLine();
+        }
+
+
+    }
+
+    public class Lienzo
+    {
+        int _x;
+        int _y;
+        int _alto;
+        int _ancho;
+
+        #region Constructor
+
+        public Lienzo()
+        {
+
+        }
+
+        public Lienzo(int x, int y, int alto, int ancho)
+        {
+            _x = x;
+            _y = y;
+            _alto = alto;
+            _ancho = ancho;
+        }
+
+        #endregion
+
+        #region Metodos
+        public void Dibuja()
+        {
+            Console.WriteLine("Preparando el LIENZO");
+        }
+        #endregion
+        /*Aqui más metodos, propiedades y otros miembros etc...*/ 
+    }
+
+    public class LienzoImpresora : Lienzo
+    {
+        string _color;
+
+        #region Constructor
+        public LienzoImpresora(int x, int y, int alto, int ancho,string color):base(x,y,alto,ancho)//En este caso la palabra reservada "BASE" va a llamar al constructor de la clase que hemos heredado con los parametros indicados, pasandoselos a nuestro nuevo constructor, por lo que todos tendran valor excepturando uno de ellos
+        {
+            _color = color;
+        }
+
+        public LienzoImpresora()
+        { 
+        
+        }
+        #endregion
+
+        #region Metodos
+        /*
+         NEW -> Usamos la palabra reservada 'NEW', para diferenciar el metodo Dibuja() 
+         de la clase LienzoImpresora de la de la clase Lienzo Dibuja() que estamos heredando
+         */
+        public new void Dibuja()
+        {
+            /*
+             BASE -> Usamos la palabra reservada 'BASE' para llamar al  metodo
+             de la clase que heredamos que se llama igual que uno de nuestra clase actual
+             
+             */
+            base.Dibuja();
+            Console.WriteLine("Dibujando la IMPRESORA");
+        }
+        #endregion
+    }
+}
---
+++ Tema7/AppPropiedades/AppPropiedades/Program.cs
@@ -0,0 +1,112 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola:Ejemplo de uso de las propiedades de lectura y escritura Get y Set
+ Fecha: 15/01/2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPropiedades.pbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ficha f1 = new Ficha();
+            f1.Nombre = "Pepe";
+            f1.Edad = 22;
+            f1.Nota= 5.8F;
+            Console.WriteLine(f1.ToString());
+
+            Ficha f2 = new Ficha();
+            f2.Nombre = "Manolo";
+            f2.Edad = 27;
+            f2.Nota = 16.8F;
+            Console.WriteLine(f2.ToString());
+            Console.ReadLine();
+        }
+    }
+
+    public class Ficha
+    {
+        #region Campos
+
+        string _nombre;
+        int _edad;
+        float _nota;
+
+        #endregion
+
+        #region Propiedades
+
+        public string Nombre
+        {
+            //Lectura
+            get {return _nombre;}
+            //Escritura
+            set
+            {
+                #region Filtros
+                if (value == string.Empty)
+                    value = "SIN NOMBRE";
+                           
+                #endregion
+                _nombre = value; 
+                 }
+        }
+
+        public int Edad
+        {
+            //Lectura
+            get { return _edad; }
+            //Escritura
+            set { _edad = value; }
+        }
+
+        public float Nota
+        {
+            //Lectura
+            get { return _nota; }
+            //Escritura
+            set
+            {
+                #region Filtros
+                if (value > 10)
+                    value = 10;
+                if (value < 0)
+                    value = 0;
+                #endregion
+                _nota = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Ficha()
+        {
+
+        }
+        public Ficha(string nombre, int edad, float nota)
+        {
+            _nombre = nombre;
+            _edad = edad;
+            _nota = nota;
+        }
+
+        #endregion
+
+        #region Metodos
+
+        //Palabra reservada "OVERRIDE" es utilidaza para sobreescribir metodos comoe ne ste caso ToString()
+        public override string ToString()
+        {
+            return _nombre + ";" + _edad.ToString() + ";" + _nota;
+        }
+
+        #endregion
+    }
+}
