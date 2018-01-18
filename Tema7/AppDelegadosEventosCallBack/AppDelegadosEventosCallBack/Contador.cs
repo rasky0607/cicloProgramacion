7326efc41d4b937aa5994740802424e142084eba
Pablo Lopez <esviasalpa@gmail.com>
2018-01-18 10:39:06 +0100

    Ejemplo de Eventos con varias clases usando delegados

---
+++ Tema7/AppDelegadosEventosCallBack/AppDelegadosEventosCallBack/Contador.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Añadido
+using System.Threading;
+
+namespace AppDelegadosEventosCallBack
+{
+    class Contador
+    {
+        #region Delegados
+
+        //Declaracion del delegado
+        public delegate void CambiaContador();
+
+        #endregion
+
+        #region Eventos
+        //Sintaxys evento 1->Ambito palabra 2->reservada 3->nombre del delegado en el que esta basado el evento 4-> Nombre del evento
+        public event CambiaContador cambioValor;
+
+        public event CambiaContador cambioValor5;//Cuando sea multiplo de 5
+        #endregion
+
+        #region Campos
+
+        int _contador;
+        bool _iniciar;     
+        int _fin;
+
+        #endregion
+
+        #region Propiedades
+
+        public bool Iniciar
+        {
+            get { return _iniciar; }
+            set { _iniciar = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+        public Contador()
+        {
+            _contador = 0;
+            _iniciar = false;
+            _fin = 100;
+        }
+        #endregion
+
+        #region Metodos
+
+        public void VerContador()
+        {
+            if (!_iniciar)//Si vale falso, no hagas nada
+                return;
+            for (int i = 0; i < _fin; i++)
+            {
+                Thread.Sleep(500);//Para usar Thread añadimos el espacio de  nombre->using System.Threading;
+                Console.SetCursorPosition(10, 10);
+                Console.Write(_contador++);
+
+                //Lanzamos el evento **(si no preguntamos si es distinto de null, el evento siempre estaria lanzandose..)**
+                if (cambioValor != null)//si alguien se aputno al evento, fuera de esta clase
+                    cambioValor();
+                if (cambioValor5 != null && _contador % 5 == 0)//si alguien se aputno al evento, fuera de esta clase y el contador es multiplo de 5
+                    cambioValor5();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Tema7/AppDelegadosEventosCallBack/AppDelegadosEventosCallBack/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola:Ejemeplo de eventos y la llamada inversa que realizan "Call Back" desde otra clase a la principal
+ ---DESCRIPCION DE LO QUE HAREMOS:
+ * crearemos una clase "Contador" 
+ ----En la clase Contador---
+ * Crearemos nuestros Delegados"cambioValor" y "cambioValor5"
+ * Crearemos unos cambios y un Constructor de la clase, para iniciar un contador cuando 
+ * con la propiedad "Iniciar" del campo _iniciar cambie a "true"
+ *Creamos un metodo llamado "VerContador()" que incrementara el valor de una de las propiedades
+ *Lanzaremos los eventos
+ -------------------------
+ -----En la clase Principal---
+ * Instanciamos un objeto de la clase
+ * Nos apuntamos a los eventos
+ * Escribimos los metodos "c1_cambioValor" y "c1_cambioValor5"  que se van a ejecutar cuando se lance dichos eventos
+ * a los que nos apuntamos.
+ ------FIN DESCRIPCIONS-------
+ *
+ 
+ Fecha:18/01/2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppDelegadosEventosCallBack.pbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contador c1 = new Contador();
+            //Nos apuntamos al metodo igualandole el al metodo"c1_cambioValor"(que es lo que va ocurrir) o que se va ejecutar cuando se produzca dicho evento"cambioValor"
+            c1.cambioValor += c1_cambioValor;//Truco para que loe scriba automatico, escribimos el nombre del evento += y tabulamos dos veces
+
+            c1.cambioValor5 += c1_cambioValor5;
+
+            //Para inciar el contador hay que cambiar el valor dela propiedad _iniciar
+            c1.Iniciar = true;
+            c1.VerContador();
+
+
+            Console.ReadLine();
+        }
+
+     
+        #region Que va ocurrir cuando se lanza y me apunto al evento
+
+        static bool ahora = true;//para que la primera vez pinte en la primera posicion
+        static int incrementaX = 0;//para que despues de la primera vez avance la posicion de X
+        //Lo que va ocurrir cuando se lanze el evento al que nos hemos apuntado
+        static void c1_cambioValor()
+        {
+            if(ahora)
+
+                if (ahora)
+                {
+                    Console.SetCursorPosition(incrementaX, 20);
+                    ahora = false;
+                }
+            Console.SetCursorPosition(incrementaX++, 20);
+            Console.Write("-");
+        }
+
+
+        //Lo que va ocurrir cuando se lanze el evento al que nos hemos apuntado
+        static void c1_cambioValor5()
+        {
+            Console.SetCursorPosition(5, 5);
+            Console.Write(" Eh!, es multiplo de 5!");
+            
+        }
+
+
+        #endregion
+    }
+}
