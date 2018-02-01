0828620550aa211f05fd679cf633410e8ad91001
Pablo Lopez <esviasalpa@gmail.com>
2018-02-01 10:28:07 +0100

    Sobrecarga de Operadores y utilizacion de estos

---
+++ Tema7/AppSobreCargaOperadores/AppSobreCargaOperadores/Agenda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSobreCargaOperadores
+{
+    class Agenda
+    {
+        const int NUMEROMAXENTRADAS = 100;
+        string[] anotaciones = new string[NUMEROMAXENTRADAS];
+        int contadorAnotaciones = 0;//numero de anotaciones realizadas en el array
+
+        public void ListarAnotaciones()
+        {
+            for (int i = 0; i < contadorAnotaciones; i++)            
+                Console.WriteLine(anotaciones[i]);
+
+            Console.Write(" Fin anotaciones... ");
+            Console.ReadLine();
+        }
+
+        //Para añadir  anotaciones vamos a sobrecargar el operador +
+        public static bool operator +(Agenda unaAgenda, string unaAnotacion)
+        {
+            if (unaAgenda.contadorAnotaciones == NUMEROMAXENTRADAS)//Si no entra mas anotaciones
+                return false;
+            else
+            {
+                unaAgenda.anotaciones[unaAgenda.contadorAnotaciones++] = unaAnotacion;
+                return true;
+            }
+        }
+    }
+}
---
+++ Tema7/AppSobreCargaOperadores/AppSobreCargaOperadores/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola:SobreCargar Operadores  para dar distintas funcionalidades a determinados operadores.
+ Con una Clase agenda que añade apuntes o anotaciones.
+ Fecha:31/01/2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSobreCargaOperadores.pbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agenda miagenda = new Agenda();
+           
+            /*El valor  boolean que devuelve nuestro metodo que sobrecarga el Operador.
+            debe usarse si o si , por eso razon interrogamos por el .*/
+            if (miagenda + " Mi primer contacto")
+                                     
+            miagenda.ListarAnotaciones();
+
+
+        }
+    }
+}
