7ee46d800c7067053fc8058e74767c101fe3c6b5
Pablo Lopez <esviasalpa@gmail.com>
2018-02-05 10:22:07 +0100

    Clonacion profunda y simple de Objetos

---
+++ Tema7/AppClonacionDeObjetos/AppClonacionDeObjetos/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola:Ejemplo de clonacion de Objetos usando la colnacion Simple o superficial
+ * y la clonacion Profunda(usando una clase Punto yotra Rectangulo)
+ Fecha:05/02/2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//añadido
+using AppGestionarPersonas;
+
+namespace AppClonacionDeObjetos.pbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Copia Simple o superficial
+            Persona pOrigen = new Persona("Nombre1", "Apellido1", DateTime.Now, 1.0);
+            Persona PCopia = (Persona)pOrigen.Clone();//Necesitamos el casting ha persona , ya que nuestro metodo devuelve un objeto,**El casting realizado en nuestro metodo clone (es para realizar la copia,pero realmente sigue devolviendo un objeto luego)
+            Console.WriteLine("\n -Objt Persona Original\n");
+           Console.WriteLine(pOrigen.ToString());
+           Console.WriteLine("\n -Objt Persona Copiado del Original\n");
+           Console.WriteLine(PCopia.ToString());
+           Console.WriteLine("\n --Comprobacion de que es una copia correcta y uno no cambia datos del otro --\n");
+            //comprobamos que al cambiar los datos de uno, no cambiamos los datos del otro objeto.
+           pOrigen.Nombre = "JuanOrigen";
+           PCopia.Nombre = "PepitoCopia";
+           Console.WriteLine(pOrigen.ToString());
+           Console.WriteLine(PCopia.ToString());
+            #endregion
+
+           Console.ReadLine(); 
+           Console.Clear();
+
+           #region Probando la Clase Rectangulo y Punto
+           Console.WriteLine("\n Comprobaciond e la clase Punto y Rectangulo\n ");
+           Punto p1 = new Punto(5, 5);
+           Punto p3 = new Punto(20, 10);
+           Rectangulo r1 = new Rectangulo(p1, p3);
+           r1.InformacionPunto();
+           r1.Dibuja();
+           #endregion
+
+           Console.ReadLine();
+           Console.Clear();
+           #region Copia Profunda
+           Console.WriteLine("\n Copia profunda con la calse Punto y Rectangulo\n");
+           Rectangulo r2copia = (Rectangulo)r1.Clone();
+           r2copia.puntos[0].X = 0;
+           r2copia.puntos[0].Y = 0;
+           r2copia.InformacionPunto();
+           r2copia.Dibuja();
+
+           Console.Clear();
+            //Mostrar El original para probar que se realizo la copia correctamente
+           r1.InformacionPunto();
+           r1.Dibuja();
+
+           #endregion
+
+
+           Console.ReadLine();
+            
+            
+        }
+    }
+}
---
+++ Tema7/AppClonacionDeObjetos/AppClonacionDeObjetos/Punto.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppClonacionDeObjetos
+{
+    
+    class Rectangulo:ICloneable
+    {
+
+        ConsoleColor color;//color de relleno  del  rectangulo
+        char textura;
+       public Punto[] puntos;//Los 4 puntos  que correcponde a los vertices del rectangulo
+
+        #region Constructor
+
+       public Rectangulo()
+       {
+           puntos = new Punto[4];
+           textura = ' ';
+           color = ConsoleColor.Red;
+       }
+
+       public Rectangulo(Punto p1, Punto p3)//conociendo el uno(Superior Izquierdo) y conociendo el 3(Inferior derecho) calculamos los otros dos
+       {
+           Punto p2 = new Punto(p3.X, p1.Y);  
+           Punto p4 = new Punto(p1.X,p3.Y );
+           puntos = new Punto[4] { p1, p2, p3, p4 };
+           textura = ' ';
+           color = ConsoleColor.Red;          
+           
+       }
+
+        #endregion
+
+       /// <InformacionPunto>
+        /// Escribe por p antalla informacion sobre los  puntos.
+       /// </InformacionPunto>
+       public void InformacionPunto()
+       {
+           Console.WriteLine("p1: {0},\t p2:{1},\t p3:{2},\t p4:{3} ", puntos[0], puntos[1], puntos[2], puntos[3]);
+           Console.WriteLine("Color: {0}, \t\t Textura: {1}", color, textura);
+       }
+       public void Dibuja()
+       {
+           char caracter = textura;
+           ConsoleColor colorEntrada = Console.BackgroundColor;
+           Console.BackgroundColor = color;
+           int x1 = puntos[0].X;//punto p1
+           int y1 = puntos[1].Y;//punto p3 los punto iniciales que obtenemos
+           int x2 = puntos[2].X;
+           int y2 = puntos[3].Y;
+
+           for (int i = x1; i <= x2; i++)
+           {
+               for (int j = y1; j <= y2; j++)
+               {
+                   Console.SetCursorPosition(i, j);
+                   Console.Write(caracter);
+               }
+               
+           }
+           Console.BackgroundColor = colorEntrada;
+
+       }
+
+
+        //Copia Profunda
+       public object Clone()
+       {
+          Rectangulo c = new Rectangulo();
+          c = (Rectangulo)this.MemberwiseClone();
+          for (int i = 0; i < puntos.Length; i++)//Aqui es donde radica la copia profunda ya qeue estamos copiando uan estructura por referencia de un lado a otro.         
+              c.puntos[i] = this.puntos[i];
+          
+
+          return c;
+       }
+
+    }
+
+    class Punto
+    {
+        // representacion de Puntos en el espacio
+        #region Campos
+        int x;
+        int y;
+        #endregion
+
+        #region Propiedades
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        #endregion
+
+        public Punto()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Punto(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}-{1}", x, y);
+        }
+    }
+}
