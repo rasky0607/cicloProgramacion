261624c293ed147ca1e65ddc95f24c10040d9516
Pablo Lopez <esviasalpa@gmail.com>
2018-02-05 10:22:59 +0100

    Interfaces para implementar la instrucion Foreach con nuestra clases

---
+++ Tema7/AppIenumeratorIenumerable/AppIenumeratorIenumerable/Lista.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//añadido 
+using System.Collections;
+
+namespace AppIenumeratorIenumerable
+{
+    class Lista:IEnumerator,IEnumerable
+    {
+        int[] numeros = { 1, 3, 5, 99, 110, 4, 7 };
+        int posicion = -1; //ya que el iEnumerator y I enumerable , lo va incrementar en 1 y la primera posicion deuna lista o un array es 0
+
+        #region IEnumerator
+        public object Current
+        {
+            //Devuelve el elemento de la posicion actual
+            get { return numeros[posicion]; }
+        }
+
+        public bool MoveNext()
+        {
+           /*Desplaza  el indice  que referencia la posicion al siguiente valor para desplazarse  al siguiente elemneto
+            devuelve: true si avanzo
+                     false si se alcanzo el final
+            */
+
+            if (posicion < numeros.Length - 1)
+            {
+                posicion++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+
+        }
+
+        public void Reset()
+        {
+            //Reinicia el indice al comienzo o el pricipio el cual debe ser -1 ya que luego lo incrementara en 1 y avanzara ala primera posicion la "0"
+            posicion = -1;
+        }
+        #endregion
+
+        #region IEnumerable
+        public IEnumerator GetEnumerator()
+        {
+            //devuelve  tipo IEnumerable a la clase
+           return this;
+        }
+        #endregion
+    }
+}
---
+++ Tema7/AppIenumeratorIenumerable/AppIenumeratorIenumerable/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola: Ejemplos con los interfaces IEnumerable y Ienumerator 
+ * para poder usar la instruccion Foreach con las instancias de una clase nuestra
+ Fecha:05/02/2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Añadido para poder implementar el Ienumerable sin tipos.
+using System.Collections;
+
+namespace AppIenumeratorIenumerable.pbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista milista = new Lista();
+            foreach (int tmp in milista)
+            {
+                Console.Write("\t {0}", tmp);
+            }
+            Console.ReadLine();
+        }
+
+       
+    }
+}
