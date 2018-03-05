9bbc852a3559b3d4961fdce1c9811c3e8e0de835
Pablo Lopez <esviasalpa@gmail.com>
2018-03-05 10:31:29 +0100

    Uso de propiedad de la clase File para poder ver los atributos de un determinado fichero

---
+++ Tema8/AppAtributosFicheros/AppAtributosFicheros/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ Autor: Pablo Lopez
+ Ejemplo consola:Ejemeplo de lectura y modificacion de atributos de un fichero
+ Fecha:22/01/2018
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//añadido
+using System.IO;
+
+namespace AppAtributosFicheros.pbl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ruta = @"c:\basura\prueba.txt";
+            string texto = "\r\n Hola caracola";
+
+            Console.WriteLine(" Atributos: "+File.GetAttributes(ruta));//Si tiene permisos de escritura y es visible, dira que es de tipo Archive
+            File.SetAttributes(ruta, FileAttributes.Normal);
+            Console.WriteLine(" Atributos: " + File.GetAttributes(ruta));
+            Console.WriteLine(File.ReadAllText(ruta));
+            Console.ReadLine();
+        }
+    }
+}
