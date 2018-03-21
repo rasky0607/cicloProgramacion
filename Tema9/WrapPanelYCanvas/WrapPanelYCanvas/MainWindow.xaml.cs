74d15e14e1dffb2234a84a9b2ea39c1ab4d1abb4
Pablo Lopez <esviasalpa@gmail.com>
2018-03-21 11:02:11 +0100

    Ejemplo de caracteristicas de contenedores WrapPanel y Canvas

---
+++ Tema9/WrapPanelYCanvas/WrapPanelYCanvas/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WrapPanelYCanvas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Crear un control rectangulo
+            Rectangle _rect = new Rectangle();
+            _rect.Width = 50;
+            _rect.Height = 50;
+            _rect.Fill = new SolidColorBrush(Colors.Blue);          
+            //Especifica las posiciones del elemento dentro del canvas
+            Canvas.SetLeft(_rect, 20);//(Porpiedad de dependencia
+            Canvas.SetTop(_rect, 10);
+            //Añadir al canvas un elemento
+            cvContenedor.Children.Add(_rect);
+        }
+        
+    }
+}
