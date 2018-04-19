ee67b01e310007b146277d2b16a84f32de6caf73
Pablo Lopez <esviasalpa@gmail.com>
2018-04-19 10:34:13 +0200

    intercambio de informacion entre vantanas con la clase App con propiedades

---
+++ Tema9/VariasVentanasWpf/VariasVentanasWpf/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VariasVentanasWpf
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        //Propiedad
+        public string Compartir
+        {
+            get;
+            set;
+        }
+
+        private void Application_Activated(object sender, EventArgs e)
+        {
+            Compartir = "hola Caracola";//imagenemos ahora que lo guardemos ahora en la porpiedad compartir, sera lo que hay en el textbox de otra ventan como Window2.text
+        }
+    }
+}
---
+++ Tema9/VariasVentanasWpf/VariasVentanasWpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VariasVentanasWpf
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            //Creamos un objeto de App  donde guardaremos el objeto de la aplicacion actual que esta en ejecucion
+            App objApp = Application.Current as App;//La palabra "as"(solo se puede usar con tipos de datos por referencia es como un cating una conversion como si tubiera un try, si puede lo hace, si no, no
+            if (objApp != null)//Si esta lleno o mi objeto que estoy creando apunta a App
+            {
+                tbxOrigen.Text = objApp.Compartir;// Lo que esta en la porpiedad compartir lo guardamos en  la propiedad de textbox
+            }
+        }
+    }
+}
