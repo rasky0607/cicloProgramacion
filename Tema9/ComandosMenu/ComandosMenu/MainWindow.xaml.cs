fd45d1030e8d4f1e25a9f06dad06353f33394b9d
Pablo Lopez <esviasalpa@gmail.com>
2018-04-19 10:39:55 +0200

    Ejemplo de comandos con una barra de herramientas  y de estado con comandos de copiar pegar cortar etc..

---
+++ Tema9/ComandosMenu/ComandosMenu/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComandosMenu
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            //comprueba si esta habilitado o no
+            e.CanExecute = true;//Habilita el comando y el control en el que esta asociado.
+        }
+
+        private void OpenCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //Lo que va realizar el comando
+            tbxMensaje.Text = " Estas usando el comando Open...";
+        }
+
+        private void CloseCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;//Habilita el comando y el control en el que esta asociado.
+        }
+
+        private void CloseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //Lo que va realizar el comando
+            tbxMensaje.Text = " Estas usando el comando Cerrar...";
+        }
+    }
+}
