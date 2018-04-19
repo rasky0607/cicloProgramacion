1361ef316fbbb5a045aa0e768ca9140e7cc9b6e7
Pablo Lopez <esviasalpa@gmail.com>
2018-04-19 10:35:14 +0200

    Intercambio de informacion entre ventans con eventos y delegados sin usar la clase App

---
+++ Tema9/IntercambiosDatosEntreVentanas/IntercambiosDatosEntreVentanas/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntercambiosDatosEntreVentanas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Crea una instancia del formulario que queremos abrir
+            Window2 w2 = new Window2();
+            w2.OnEnviar += w2_OnEnviar;
+            w2.ShowDialog();//abre la ventana 2
+        }
+
+        //metodo que se ejecutara tras producirse el evento OnEnviar
+        void w2_OnEnviar(string[] datos)
+        {
+            tbxdato1.Text = datos[0];
+            tbxdato2.Text = datos[1];
+        }
+    }
+}
---
+++ Tema9/IntercambiosDatosEntreVentanas/IntercambiosDatosEntreVentanas/Window2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IntercambiosDatosEntreVentanas
+{
+    /// <summary>
+    /// Lógica de interacción para Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        //En primer lugar crearemos un delegado
+        public delegate void dlgEnviar(string[] datos);
+        //En segundo lugar creamos un evento basado ene la nterior delegado.
+        public event dlgEnviar OnEnviar;
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            string[] datos = { tbxdato1.Text, tbxdato2.Text };
+            if (OnEnviar != null)//Si alguien se apunto a evento
+            {
+                OnEnviar(datos);
+            }
+        }
+    }
+}
