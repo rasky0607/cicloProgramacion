ec87aefdcea349c18fa7b7ab04f42c5676c0e858
Pablo Lopez <esviasalpa@gmail.com>
2018-05-10 12:23:04 +0200

    Implementacion de INotifyPropertyChanged para cambios de propiedad

---
+++ Tema9/EjemploSimpleDeInotifypropertychanged/EjemploSimpleDeInotifypropertychanged/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Añadido
+using System.ComponentModel;//Espacio de nombre para implementar INotifyPropertyChanged
+
+namespace EjemploSimpleDeInotifypropertychanged
+{
+    class Class1:INotifyPropertyChanged
+    {
+        string _mensaje;
+
+        public string Mensaje
+        {
+            get { return _mensaje; }
+            set { _mensaje = value;
+            Notificador("Mensaje");//Notifica el cambio de la propiedad
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Metodo que ejecuta el evento PropertyChanged
+        private void Notificador(string nombrePropiedada)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(nombrePropiedada));//this el objeto que le llega , evento que recibe el nombre de la propiedad
+        }   
+    }
+}
---
+++ Tema9/EjemploSimpleDeInotifypropertychanged/EjemploSimpleDeInotifypropertychanged/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EjemploSimpleDeInotifypropertychanged
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Class1 clase1 = new Class1();
+        static int n = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dg1.DataContext = clase1;//Importante añadir el objeto creado al contexto de datos , para que el evento no apunte a null, si no a dicho objeto
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            n++;
+            clase1.Mensaje = "Este mensaje ha sido modificado " + n + " numero de veces";
+        }
+    }
+}
